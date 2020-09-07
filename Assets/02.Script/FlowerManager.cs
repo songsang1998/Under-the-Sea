57168c31c603374d2e2b2705ec25b12623239b7d
songsang1998 <44784597+songsang1998@users.noreply.github.com>
2020-09-07 12:32:49 +0900

    Signed-off-by: songsang1998 <44784597+songsang1998@users.noreply.github.com>

---
+++ Assets/02.Script/FlowerManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerManager : MonoBehaviour
+{
+    public List<Quiz_flower> quiz;
+    bool clear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+
+            quiz.Add(transform.GetChild(i).GetComponent<Quiz_flower>());
+            quiz[i].name = i - 1;
+        }
+        
+    }
+
+
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i=0; i<=8; i++ )
+        {
+            if(quiz[i].a == 1)
+            {
+                clear = true;
+                Debug.Log("clear");
+            }
+        }
+    }
+
+    public void Flowers(int s)
+    {
+        quiz[s].reverase();
+    }
+}
---
+++ Assets/02.Script/Quiz_flower.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quiz_flower : MonoBehaviour
+{
+    public int a;
+    public int name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = 0;
+        Setting();
+    }
+
+    // Update is called once per frame
+    
+    void Setting()
+    {
+        if (a == 0)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            transform.GetChild(1).gameObject.SetActive(false);
+        }
+        else
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(1).gameObject.SetActive(true);
+        }
+       
+    }
+
+    public void flower()
+    {
+        if(name%3==0)
+        {
+            if (name - 3 > 0)
+            {
+
+            }
+        }
+    }
+    public void reverase()
+    {
+        if (a == 0)
+        {
+            a = 1;
+        }
+        else
+        {
+            a = 0;
+        }
+        Setting();
+    }
+
+}
